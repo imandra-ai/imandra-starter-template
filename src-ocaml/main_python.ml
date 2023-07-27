0ad9f52066bfb87fbb5f6e6d05bdcbcf2fa15768
Koundinya Vajjha <kody@imandra.ai>
2023-07-27 10:12:56 -0700

    fix: py_gauss binding

--- src-ocaml/main_python.ml
+++ src-ocaml/main_python.ml
@@ -3,4 +3,4 @@ open Python_lib
 let () =
   if not (Py.is_initialized ()) then Py.initialize ();
   let mod_ = Py_module.create "enx_wrapper" in
-  Py_module.set mod_ "run_sim" Bindings.py_run_sim;
+  Py_module.set mod_ "run_sim" Python_bindings.Bindings.py_gauss;
